f7ece3cb88b1099ffc47ba71cfdd40a14f480428
Antonio Fransua Morales Camarena <franmorales45678@gmail.com>
2024-11-24 00:10:25 -0500

    INICION DE SESION VERIFICAR

---
+++ usuario.java
@@ -0,0 +1,19 @@
+public class usuario {
+    String user, password;
+
+    public usuario(String user , String password){
+        this.user = user;
+        this.password = password;
+    }
+
+    public String OBTENERuser(){
+        return user;
+    }
+
+    public String OBTENERpassword(){
+        return password;
+    }
+
+
+
+}
