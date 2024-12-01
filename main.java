87b1c8f0cec404f75b03fd8b215061ea17bf174a
Antonio Fransua Morales Camarena <franmorales45678@gmail.com>
2024-12-01 02:47:33 -0500

    corregido

--- main.java
+++ main.java
@@ -2,6 +2,8 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.undo.UndoableEditSupport;
+
 
 public class main {
     public static void main(String[] args) {
@@ -69,7 +71,8 @@ public class main {
                  if (usuarioCompleto.OBTENERuser().equals(USERREGISTRADO) && usuarioCompleto.OBTENERpassword().equals(CONTRAREGISTRADO)) {
                     System.out.println("BIENVENIDO:" + USERREGISTRADO);
                     ENCONTRADO = true;
-                    USERSECTOR.SECTORESDELUSER();
+                    System.out.println("-------------------------------");
+                    USERSECTOR.SECTORESDELUSER(); 
                     break;
                     
                  }   
